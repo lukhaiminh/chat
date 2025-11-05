087771549b6449bc6bf0bdd8a217d928631dea7b
MooN | Lu Khai Minh <lukhaiminh.vn@smilegate.com>
2025-11-05 13:39:51 +0700

    initial dev branch

---
+++ nuxt.config.ts
@@ -0,0 +1,53 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxtjs/mdc',
+    'nuxt-auth-utils',
+    'nuxt-charts'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  mdc: {
+    headings: {
+      anchorLinks: false
+    },
+    highlight: {
+      // noApiRoute: true
+      shikiEngine: 'javascript'
+    }
+  },
+
+  experimental: {
+    viewTransition: true
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  nitro: {
+    experimental: {
+      openAPI: true
+    }
+  },
+
+  vite: {
+    plugins: [tsconfigPaths()]
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
